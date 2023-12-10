f8a851efbac01ce513964112d4f273162eb145db
realSkilluminati <54849420+realSkilluminati@users.noreply.github.com>
2023-12-10 18:12:44 +0100

    Changed pickedVolume to be optional in case of closing the volume selection screen

--- frontend.py
+++ frontend.py
@@ -17,30 +17,38 @@ wn = Tk()
 wn.title("PiCocktailMaker")
 wn.geometry("480x800")
 
+pickedVolume = None
+
 def someFunc(drink):
     print(f"drink: {drink["name"]}")
-    pickedVolume = glassVolumes[0]
-
+    
     def dismiss():
+        global pickedVolume
+        pickedVolume = None
+
         dialog.grab_release()
         dialog.destroy()
         return
 
     def confirmVolume(vol):
+        global pickedVolume
         pickedVolume = vol
 
         dialog.grab_release()
         dialog.destroy()
-
+    
+    # Create popver
     dialog = Toplevel(wn)
     dialog.title("Glas Größe")
     dialog.geometry("480x300")
 
+    # Create Volume buttons in popover
     for i, volume in enumerate(glassVolumes):
         Button(dialog, text=f"{volume}ml", command=partial(confirmVolume, volume), height=10, width=100).grid(column=i, row=0)
 
         dialog.columnconfigure(i, weight=1)
 
+    # Configure window position to be inside the original window
     x = wn.winfo_x()
     y = wn.winfo_y()
     dialog.geometry(f"+{x}+{y+200}")
@@ -49,8 +57,10 @@ def someFunc(drink):
     dialog.transient(wn)
     dialog.wait_visibility()
     dialog.grab_set()
-
-    dialog.wait_window()
+    
+    dialog.wait_window() # Wait until popover is closed
+    print(pickedVolume)
+    if pickedVolume is None: return # Check if a volume was picked
     
     for ingredient in drink["ingredients"]:
         print(f"ingredient: {ingredient["ingredient"]}")
@@ -66,7 +76,7 @@ for i in range(0,drinkListLength):
     drink = data["drinks"][i]
 
     # Create the button for a drink
-    someButton = Button(wn, text=f"{drink["name"]}", command=partial(someFunc, drink), height = 4, width=100)
+    someButton = Button(wn, text=f"{drink["name"]}", command=partial(someFunc, drink), height = 4, width=100, bd=0, bg="black")
     someButton.grid(column=0, row=i)
 
     # Make the button-grid span the entire width
